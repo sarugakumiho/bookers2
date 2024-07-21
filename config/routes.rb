2669febf81ecae2158f22ec7b3370131af220c8c
sarugakumiho <171490497+sarugakumiho@users.noreply.github.com>
2024-07-21 17:18:11 +0000

    [Finish] 4-27

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get '/about', to: 'homes#about', authenticated: false
+  get 'home/about', to: 'homes#about', authenticated: false
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 end
